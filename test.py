5d3cd872ce07e7f6ad3c8a429061e58e5b0eacb9
rayyanhammadi <rayyan.hammedi@gmail.com>
2022-11-22 17:11:39 +0100

    test_git

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 from Main.Data_processing import Data
 from Main.Modelization import Models
 import pandas as pd
+#test_git
+
 BDD_path = "./Data/BDD_SummerSchool_BENOIT.xlsx"
 BDD_sheet = "raw_data"
 data = Data(BDD_path, BDD_sheet)
 data.data_processing()
-models = Models(data.df, 204, 12, 0)
+models = Models(data.df, 204, 15, 0)
 models.split_2()
 models.fit()
 models.predict()
