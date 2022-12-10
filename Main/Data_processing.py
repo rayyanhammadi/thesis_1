a07ea035e9d485ba96811a3e58b1382e6184e71c
rayyanhammadi <rayyan.hammedi@gmail.com>
2022-12-10 20:54:08 +0100

    added lag data feature

--- Main/Data_processing.py
+++ Main/Data_processing.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import pandas as pd
+from pprint import pprint
 
 class Data:
     def __init__(self,BDD_path, BDD_sheet):
@@ -12,12 +13,38 @@ class Data:
         self.df.columns = self.raw_df.iloc[0,:]
         self.df.set_index("dates", drop=True, inplace=True)
         self.df = self.df.astype("float")
+
         print("Data processed succesfully")
-        return(self.df)
+
+    def target(self):
+        return self.df.iloc[:,-1]
+
+    def lagged_target(self):
+        return self.lag_target(self.df.iloc[:,-1])
+
+    def covariates(self):
+        return self.df.iloc[:,:-1]
+
+    def lagged_covariates(self):
+        return pd.concat([self.lag_covariates(self.df.iloc[:,:-1]),self.lagged_target()],axis=1)
+    @staticmethod
+    def lag_covariates(data, lag=18):
+        for column in data:
+            for i in range(18):
+                data['%s_lag_%i' % (column, i+1)] = data[column].shift(i+1)
+        return data.dropna()
+
+    @staticmethod
+    def lag_target(data, lag=5):
+        y = pd.DataFrame(data)
+        for i in range(18):
+            y['Y_lag_%i' % (i+1)] = y["USA (Acc_Slow)"].shift(i+1)
+        y = y.drop(columns=["USA (Acc_Slow)"])
+        return y.dropna()
 
     def data_summary(self):
         print(self.df.head())
-        print(self.df.describe())
+        # print(self.df.describe())
 
 
 
