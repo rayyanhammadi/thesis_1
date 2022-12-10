9307c0e6cdb458623c83742a54534465696dc127
rayyanhammadi <rayyan.hammedi@gmail.com>
2022-12-10 20:54:33 +0100

    update test

--- test.py
+++ test.py
@@ -1,18 +1,24 @@
 from Main.Data_processing import Data
 from Main.Modelization import Models
+from pprint import pprint
 import pandas as pd
 #test_git
-
+pd.set_option('display.max_column',None)
+pd.set_option('display.max_rows',None)
 BDD_path = "./Data/BDD_SummerSchool_BENOIT.xlsx"
 BDD_sheet = "raw_data"
 data = Data(BDD_path, BDD_sheet)
 data.data_processing()
-models = Models(data.df, 204, 15, 0)
-models.split_2()
-models.fit()
+X_lagged = data.lagged_covariates()
+# Y = data.lagged_target()
+# print(X_lagged)
+# pprint(Y)
+models = Models(Y=data.target(), X=X_lagged, date_split=204, step_ahead=15)
 models.predict()
-#print(models.Y_test_US)
-#print(models.Y_test_US_1)
-print(models.Y_test_US_1)
-print(models.plot())
-print(models.show_matrix_confusion())
+print(models.Y_test_label)
+print(models.Y_test_label.dropna())
+models.plot()
+models.show_confusion_matrix()
+# pprint(x)
+
+#todo : corriger les indices
